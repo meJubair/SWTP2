16209f959921a74eb72631f70043f8d8583a2c56
Jubair Hossain <jubair@Jubairs-MBP.lan>
2024-03-29 11:58:09 +0200

    Jest setup and test App.tsx

---
+++ frontend/jest.config.ts
@@ -0,0 +1,27 @@
+// Jest configuration file
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    verbose: true,
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    testMatch: ["<rootDir>/jestTests/**/*.test.{js,jsx,ts,tsx}"],
+    setupFilesAfterEnv: ["<rootDir>/src/jestSetup.tsx"],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+    },
+    transformIgnorePatterns: [
+        "/node_modules/",
+        "/src/.+\\.css$" // Include .css files inside src directory
+    ],
+    //transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$"],
+    moduleNameMapper: {
+        '^@testing-library/jest-dom/extend-expect$': '<rootDir>/node_modules/@testing-library/jest-dom/',
+    },
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/src/.+\\.css$" // Exclude .css files inside src directory from coverage reports
+  ],
+};
+
+export default config;
