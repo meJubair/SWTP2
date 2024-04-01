d9ee84fca69ee38e875e351af4ad257c826198a5
Bhandari Mahesh <s2300094@BC-C02GR0BSQ6LRs-MacBook-Air.local>
2024-04-01 22:57:47 +0300

    Removed Breadcrumb and PublishedContent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CalendarContent from './components/CalendarContent';
-import PublishedContent from './components/PublishedContent';
-import Breadcrumb from './components/Breadcrumb';
 import EditorForm from './components/EditorForm';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
-        {/* Render Breadcrumb */}
-        <Breadcrumb
-          items={[
-            { label: 'Editor Form', path: '/' },
-            { label: 'Calendar Content', path: '/calendar' },
-            { label: 'Published Content', path: '/published' },
-          ]}
-        />
         {/* Route definitions */}
         <Routes>
           <Route path="/" element={<EditorForm />} />
           <Route path="/calendar" element={<CalendarContent startDate={null} endDate={null} calendarData={[]}/>} />
-          <Route path="/published" element={<PublishedContent />} />
         </Routes>
       </div>
     </Router>
