ddfd8805ba4c98bff0785f711318dd481265d643
Bhandari Mahesh <s2300094@BC-C02GR0BSQ6LRs-MacBook-Air.local>
2024-04-01 23:05:03 +0300

    Generate calender grid with form data

--- frontend/src/components/EditorForm.tsx
+++ frontend/src/components/EditorForm.tsx
@@ -11,6 +11,7 @@ interface FormData {
   endDate: Date | null;
 }
 
+
 const EditorForm: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({
     author: '',
@@ -20,6 +21,7 @@ const EditorForm: React.FC = () => {
   });
   const [calendarData, setCalendarData] = useState<Date[]>([]);
   const [error, setError] = useState<string>('');
+ 
 
   const handleChange = (name: keyof FormData) => (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
