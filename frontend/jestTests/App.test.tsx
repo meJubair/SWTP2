a2620a421a409cb6ea2c53bc13b64f7cbd1f7278
Jubair Hossain <jubair@Jubairs-MBP.lan>
2024-04-03 10:33:02 +0300

    Added import stataements test and React

--- frontend/jestTests/App.test.tsx
+++ frontend/jestTests/App.test.tsx
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 import App from '../src/App';
+import {test} from '@jest/globals';
+import React from 'react';
 
 test('renders App component', () => {
   render(<App />);
