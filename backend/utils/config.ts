d927c1cf5154ca8e2d845035ce6c30fc817450a3
JerryUusis <jerry.uusitalo@gmail.com>
2024-04-04 13:35:48 +0300

    Add FIREBASE_API_KEY

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -2,5 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY
 
-export { PORT };
+export { PORT, FIREBASE_API_KEY };
