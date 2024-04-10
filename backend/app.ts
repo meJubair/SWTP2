db13190303509276970ab71ae1ecad49dcf3852d
JerryUusis <jerry.uusitalo@gmail.com>
2024-04-10 14:47:08 +0300

    Install CORS

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,10 @@
 import helmet from "helmet";
 import express from "express";
+import cors from "cors";
 const app = express();
 import calendarRouter from "./controllers/calendar";
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use("/api/calendars", calendarRouter);
