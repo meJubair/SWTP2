48ccd9e1ac7ac25897141a1c63d88dead3a06b51
JerryUusis <jerry.uusitalo@gmail.com>
2024-04-10 11:31:25 +0300

    Create endpoint for authenticating new user in Firebase

--- backend/controllers/calendar.ts
+++ backend/controllers/calendar.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
-import { getCalendarDataFromFirebase } from "../services/firebaseService";
+import {
+  getCalendarDataFromFirebase,
+  registerWithEmailAndPassword,
+} from "../services/firebaseService";
 
 const calendarRouter = express.Router();
 
@@ -7,11 +10,23 @@ calendarRouter.get("/", async (request: Request, response: Response) => {
   try {
     const calendarData = await getCalendarDataFromFirebase();
     response.json(calendarData);
-    return calendarData;
   } catch (error) {
     console.error("Error when fetching calendar data:", error);
-    response.status(500).json({ error: "Internal server error"})
+    response.status(500).json({ error: "Internal server error" });
   }
 });
+
+calendarRouter.post(
+  "/createuser",
+  async (request: Request, response: Response) => {
+    try {
+      const { username, email, password } = request.body;
+      await registerWithEmailAndPassword(username, email, password);
+      response.status(200).end("success");
+    } catch (error) {
+      response.status(500).json({ error: "Internal server error" });
+    }
+  }
+);
 
 export default calendarRouter;
