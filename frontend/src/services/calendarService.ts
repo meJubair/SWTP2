1af77332b2cf13649e56cc6c9d82035d2e844bff
JerryUusis <jerry.uusitalo@gmail.com>
2024-04-10 15:14:46 +0300

    Update baseUrl

--- frontend/src/services/calendarService.ts
+++ frontend/src/services/calendarService.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const baseUrl: string = "http://localhost:3001/api/calendars"
+
 const registerUser = async (userObject: {
   username: string;
   email: string;
   password: string;
 }) => {
   try {
-    await axios.post(`http://localhost:3001/api/calendars/register`, userObject);
+    await axios.post(`${baseUrl}/register`, userObject);
     console.log("User registered successfully")
   } catch (error) {
     console.log("Error registering user:", error);
