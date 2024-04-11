af00ff3cd413eaa6d9143db9b9d670623830c13d
JerryUusis <jerry.uusitalo@gmail.com>
2024-04-11 14:29:58 +0300

    Refactor loginUser function to return the response

--- frontend/src/services/calendarService.ts
+++ frontend/src/services/calendarService.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const baseUrl: string = "http://localhost:3001/api/calendars"
+const baseUrl: string = "http://localhost:3001/api/calendars";
 
 const registerUser = async (userObject: {
   username: string;
@@ -9,22 +8,19 @@ const registerUser = async (userObject: {
 }) => {
   try {
     await axios.post(`${baseUrl}/register`, userObject);
-    console.log("User registered successfully")
+    console.log("User registered successfully");
   } catch (error) {
     console.log("Error registering user:", error);
   }
 };
 
-const loginUser = async (userObject: { email: string, password: string}) => {
+const loginUser = async (userObject: { email: string; password: string }) => {
   try {
     const response = await axios.post(`${baseUrl}/login`, userObject);
-    if (response.status === 200) {
-      return console.log("Login succesful")
-    }
+    return response;
+  } catch (error) {
+    console.error("Login failed:", error);
   }
-  catch(error) {
-    console.log("Login failed:", error)
-  }
-}
+};
 
 export { registerUser, loginUser };
