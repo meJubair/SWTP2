4e446e4de43fa06b2d4bcfeaceb6056bd9192efb
George Uzoh <ugochukwuuzoh@gmail.com>
2024-04-18 14:31:32 +0300

    comment out unused code

--- frontend/src/components/TextConfig.tsx
+++ frontend/src/components/TextConfig.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Slider from '@mui/material/Slider';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Slider from "@mui/material/Slider";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 export interface TextConfigType {
   title: string;
@@ -30,7 +30,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
           label="Title"
           fullWidth
           value={values.title}
-          onChange={(e) => onChange('title', e.target.value)}
+          onChange={(e) => onChange("title", e.target.value)}
         />
       </Grid>
       <Grid item xs={12}>
@@ -38,7 +38,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
           label="Subtitle"
           fullWidth
           value={values.subtitle}
-          onChange={(e) => onChange('subtitle', e.target.value)}
+          onChange={(e) => onChange("subtitle", e.target.value)}
         />
       </Grid>
       <Grid item xs={12}>
@@ -46,7 +46,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
           label="Description"
           fullWidth
           value={values.description}
-          onChange={(e) => onChange('description', e.target.value)}
+          onChange={(e) => onChange("description", e.target.value)}
           multiline
           minRows={4}
         />
@@ -55,7 +55,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
         <Typography variant="h6">Font Size</Typography>
         <Slider
           value={values.fontSize}
-          onChange={(e, value) => onChange('fontSize', value as number)}
+          // onChange={(e, value) => onChange('fontSize', value as number)}
           min={8}
           max={72}
           step={1}
@@ -68,7 +68,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
         <Typography variant="h6">Font Weight</Typography>
         <Select
           value={values.fontWeight}
-          onChange={(e) => onChange('fontWeight', e.target.value as string)}
+          onChange={(e) => onChange("fontWeight", e.target.value as string)}
           fullWidth
         >
           <MenuItem value="normal">Normal</MenuItem>
@@ -79,7 +79,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
         <Typography variant="h6">Font Family</Typography>
         <Select
           value={values.fontFamily}
-          onChange={(e) => onChange('fontFamily', e.target.value as string)}
+          onChange={(e) => onChange("fontFamily", e.target.value as string)}
           fullWidth
         >
           <MenuItem value="Arial">Arial</MenuItem>
@@ -91,7 +91,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
           label="Background Color"
           type="color"
           value={values.backgroundColor}
-          onChange={(e) => onChange('backgroundColor', e.target.value)}
+          onChange={(e) => onChange("backgroundColor", e.target.value)}
           fullWidth
         />
       </Grid>
@@ -100,7 +100,7 @@ const TextConfig: React.FC<TextConfigProps> = ({ onChange, values }) => {
           label="Text Color"
           type="color"
           value={values.textColor}
-          onChange={(e) => onChange('textColor', e.target.value)}
+          onChange={(e) => onChange("textColor", e.target.value)}
           fullWidth
         />
       </Grid>
