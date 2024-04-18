ad252cdd0b610be9f5afc857770f066e9ebe74fa
George Uzoh <ugochukwuuzoh@gmail.com>
2024-04-18 14:17:15 +0300

    add routes to calendar data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import EditorViewMain from "./routes/EditorViewMain";
 import Root from "./routes/Root";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import EditorViewSingle from "./routes/EditorViewSingle";
 
 const App: React.FC = () => {
   return (
@@ -25,7 +26,12 @@ const App: React.FC = () => {
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/calendars" element={<Calendars />} />
-              <Route path="/editor" element={<EditorViewMain />} />
+              <Route path="/calendars/:new" element={<EditorViewMain />} />
+              <Route path="/calendars/:title" element={<EditorViewMain />} />
+              <Route
+                path="/calendars/:title/:id"
+                element={<EditorViewSingle />}
+              />
             </Route>
           </Routes>
         </Router>
--- frontend/src/components/CalendarDoor.tsx
+++ frontend/src/components/CalendarDoor.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import { Link } from "react-router-dom";
 
 interface CalendarDoorProps {
   value: Date;
+  dateArray: Date[];
 }
 
-const CalendarDoor: React.FC<CalendarDoorProps> = ({ value }) => {
+const CalendarDoor: React.FC<CalendarDoorProps> = ({ value, dateArray }) => {
+  const doorId = dateArray.indexOf(value);
+
   return (
     <Box sx={{ background: "#d9d9d9", padding: "20px", borderRadius: "5px" }}>
       {value.toLocaleDateString()}
+      <Link to={`${doorId + 1}`}>
+        <Box onClick={() => console.log("Door index:", doorId)}>Edit</Box>
+      </Link>
     </Box>
   );
 };
