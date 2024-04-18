0b2facfa87fb0d867344da62f724f6d31e1ac688
JerryUusis <jerry.uusitalo@gmail.com>
2024-04-18 14:14:38 +0300

    Add setCalendars reducer

--- frontend/src/store/calendarSlice.ts
+++ frontend/src/store/calendarSlice.ts
@@ -1,11 +1,16 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const calendarSlice = createSlice({
-    name: 'calendar',
-    initialState: {
-        calendars: [],
+  name: "calendar",
+  initialState: {
+    calendars: [],
+  },
+  reducers: {
+    setCalendars: (state, action) => {
+      state.calendars = action.payload;
     },
-    reducers: {}
-})
+  },
+});
 
+export const { setCalendars } = calendarSlice.actions;
 export default calendarSlice.reducer;
