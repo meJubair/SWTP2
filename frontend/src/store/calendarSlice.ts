84f1fbcb95f57f33861b3aed2ceebf1bc3836cdc
George Uzoh <ugochukwuuzoh@gmail.com>
2024-04-23 12:54:52 +0300

    refactor app to update existing calendar title

--- frontend/src/store/calendarSlice.ts
+++ frontend/src/store/calendarSlice.ts
@@ -1,16 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CalendarData } from "../../../backend/types/calendarInterface";
+
+interface CalendarState {
+  calendars: CalendarData[];
+}
+
+const initialState: CalendarState = {
+  calendars: [],
+};
 
 export const calendarSlice = createSlice({
   name: "calendar",
-  initialState: {
-    calendars: [],
-  },
+  initialState,
   reducers: {
     setCalendars: (state, action) => {
       state.calendars = action.payload;
     },
+    setCalendarTitle: (state, action: PayloadAction<{calendarIndex: number, newTitle: string}>) => {
+      const { calendarIndex, newTitle } = action.payload;
+      state.calendars[calendarIndex].title = newTitle;
+    },
+    setStartDate: (state, action) => {
+      const { calendarIndex, newStartDate } = action.payload;
+      state.calendars[calendarIndex].startDate = newStartDate;
+    },
+    setEndDate: (state, action) => {
+      const { calendarIndex, newEndDate } = action.payload;
+      state.calendars[calendarIndex].endDate = newEndDate;
+    },
+    setCalendarDoors: (state, action) => {
+      state.calendars = state.calendars.map((calendar: CalendarData) => {
+        if (calendar.title === action.payload.title) {
+          return {
+            ...calendar,
+            calendarDoors: action.payload.calendarDoors,
+          };
+        }
+        return calendar;
+      });
+    },    
   },
 });
 
-export const { setCalendars } = calendarSlice.actions;
+export const { setCalendars, setCalendarDoors, setCalendarTitle, setStartDate, setEndDate } = calendarSlice.actions;
 export default calendarSlice.reducer;
