fc0bfbfdefd7ed7df34f1d05ef9abf79484f8f95
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-02 15:22:26 +0300

    Add rotating animation for sync icon

--- frontend/src/components/AutoSave.tsx
+++ frontend/src/components/AutoSave.tsx
@@ -2,26 +2,37 @@ import { useState } from "react";
 import Sync from "@mui/icons-material/Sync";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { keyframes } from "@emotion/react";
 
-const AutoSave = () => {
+interface AutoSaveProps {
+    syncing: boolean;
+}
+
+const AutoSave = (props: AutoSaveProps) => {
   const [isSyncing, setIsSyncing] = useState<boolean>(false);
 
+  // Implement props that get passed from EditorViewMain.tsx
+
+  const rotate = keyframes({
+    from: { transform: "rotate(360deg)" },
+    to: { transform: "rotate(0deg)" },
+  });
+
   return (
-    <Box sx={{display:"flex", justifyContent:"flex-end"}}>
+    <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
       {isSyncing ? (
         <Box sx={{ display: "flex", opacity: 0.5, gap: "0rem" }}>
-          <Typography>Changes saved</Typography>
-          <Sync />
+          <Typography>Changes saved!</Typography>
+          <Sync sx={{ animation: `${rotate} 2s` }} />
         </Box>
       ) : (
         <Box sx={{ display: "flex", opacity: 0.5, gap: "0rem" }}>
-          <Typography>Changes saved</Typography>
-          <Sync />
+          <Typography>Saving changes...</Typography>
+          <Sync sx={{ animation: `${rotate} 2s linear infinite` }} />
         </Box>
       )}
     </Box>
   );
 };
-
 
 export default AutoSave;
