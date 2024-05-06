d2405711c822089bcd837ba3447888cb3d5aba9c
George Uzoh <ugochukwuuzoh@gmail.com>
2024-05-06 10:55:46 +0300

    add calendar tags component

---
+++ frontend/src/components/CalendarTags.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { ReduxCalendarState } from "../store/stateTypes";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+interface CalendarTagsProps {
+  removeTag: (tag: string) => void;
+}
+
+const CalendarTags: React.FC<CalendarTagsProps> = ({ removeTag }) => {
+  const params: string | undefined = useParams().new;
+
+  const calendarArray = useSelector(
+    (state: ReduxCalendarState) => state.calendar.calendars
+  );
+
+  const calendarIndex = calendarArray.findIndex(
+    (calendar) => calendar.calendarId === params
+  );
+
+  const calendar = calendarArray[calendarIndex];
+
+  const calendarTags = calendar?.tags;
+
+  return (
+    <Box display="flex">
+      {}
+      {calendarTags.map((tag: string, index: number) => (
+        <Box
+          key={index}
+          sx={{
+            background: "#d9d9d9",
+            padding: "10px",
+            borderRadius: "5px",
+            margin: "5px",
+            display: "flex",
+            position: "relative",
+          }}
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "0",
+              right: "0",
+              width: "15px",
+              height: "15px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "50%",
+              cursor: "pointer",
+            }}
+            onClick={() => removeTag(tag)}
+          >
+            <Typography>x</Typography>
+          </Box>
+          <Typography>{calendarTags[index]}</Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default CalendarTags;
