b70cd2c718b2f6e058441c6a195eaab946160a94
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-06 13:47:33 +0300

    Add syncReducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
-import calendarReducer from './calendarSlice';
-import userReducer from './userSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import calendarReducer from "./calendarSlice";
+import userReducer from "./userSlice";
+import syncReducer from "./syncSlice";
 
 export default configureStore({
-    reducer: {
-        calendar: calendarReducer,
-        user: userReducer
-    }
-})
+  reducer: {
+    calendar: calendarReducer,
+    user: userReducer,
+    sync: syncReducer
+  },
+});
