0592faa29fe7784bc87cbc24f499abe7fd79caed
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-06 13:35:31 +0300

    Add setSaved reducer

--- frontend/src/store/syncSlice.ts
+++ frontend/src/store/syncSlice.ts
@@ -4,13 +4,17 @@ const syncSlice = createSlice({
   name: "syncSlice",
   initialState: {
     isTyping: false,
+    saved: true
   },
   reducers: {
     setIsTyping: (state, action) => {
       state.isTyping = action.payload;
     },
+    setSaved: (state,action) => {
+        state.saved = action.payload
+    }
   },
 });
 
-export const { setIsTyping } = syncSlice.actions;
+export const { setIsTyping, setSaved } = syncSlice.actions;
 export default syncSlice.reducer;
