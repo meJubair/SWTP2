fb8419dbc2b78e04654349c7c7f4d05631ce452d
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-06 13:31:20 +0300

    Remove props and add useSelector for save state

--- frontend/src/components/AutoSave.tsx
+++ frontend/src/components/AutoSave.tsx
@@ -3,13 +3,12 @@ import CloudDoneIcon from "@mui/icons-material/CloudDone";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { keyframes } from "@emotion/react";
+import { useSelector } from "react-redux";
+import { ReduxSyncState } from "../store/stateTypes";
 
-interface AutoSaveProps {
-  sync: boolean;
-}
-
-const AutoSave = (props: AutoSaveProps) => {
+const AutoSave = () => {
   // Implement props that get passed from EditorViewMain.tsx
+  const saved = useSelector((state:ReduxSyncState) => state.sync.saved)
 
   const rotate = keyframes({
     from: { transform: "rotate(360deg)" },
@@ -19,7 +18,7 @@ const AutoSave = (props: AutoSaveProps) => {
   return (
     <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
       <Box sx={{ display: "flex", opacity: 0.5, gap: "0.5rem" }}>
-        {props.sync ? (
+        {saved ? (
           <>
             <Typography>Changes saved! </Typography>
             <CloudDoneIcon />
