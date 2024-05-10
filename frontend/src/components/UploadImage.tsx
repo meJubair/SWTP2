6bd7f10e58e40a21564c989c1b92e1e3a0a93598
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-10 12:27:28 +0300

    Center reset button

--- frontend/src/components/UploadImage.tsx
+++ frontend/src/components/UploadImage.tsx
@@ -38,13 +38,11 @@ const UploadImage: React.FC<UploadImageProps> = ({
         sx={{ margin: "10px 0" }}
         value={imageUrl}
       />
-      <Button
-        variant="contained"
-        onClick={handleReset}
-        sx={{ margin: "10px 0" }}
-      >
-        Reset
-      </Button>
+      <Box sx={{ display: "flex", justifyContent: "center", my: "10px" }}>
+        <Button variant="contained" onClick={handleReset}>
+          Reset
+        </Button>
+      </Box>
       <Typography variant="body2">
         <Typography variant="subtitle1" component="span">
           NB:
