ececf446304b9bc5e15c4b1faa6253c1c8a8d567
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-13 12:29:18 +0300

    Add alertReducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import calendarReducer from "./calendarSlice";
 import userReducer from "./userSlice";
 import syncReducer from "./syncSlice";
+import alertReducer from "./alertSlice";
 
 export default configureStore({
   reducer: {
     calendar: calendarReducer,
     user: userReducer,
-    sync: syncReducer
+    sync: syncReducer,
+    alert: alertReducer,
   },
 });
