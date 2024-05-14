dba24dd0e7247f661f9ffd9032f399fcaebcbfe2
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-14 14:50:01 +0300

    Remove commented code

--- frontend/utils/axiosMockResponse.ts
+++ frontend/utils/axiosMockResponse.ts
@@ -12,38 +12,4 @@ const mockResponse = {
   },
 };
 
-//   status: 200,
-//   login: true,
-//   authData: {
-//     auth: {
-//       uid: "deZAYndDDXWwwbX6BV6PjuhApFO2",
-//       email: "test@gmail.com",
-//       emailVerified: false,
-//       isAnonymous: false,
-//       providerData: [
-//         {
-//           providerId: "password",
-//           uid: "test@gmail.com",
-//           displayName: null,
-//           email: "test@gmail.com",
-//           phoneNumber: null,
-//           photoURL: null,
-//         },
-//       ],
-//       stsTokenManager: {
-//         refreshToken:
-//           "AMf-vBwcxFHRM4djWE3q0fWXt3U3ciUd1zj2JOZP-Ye458kk46AxFdBRR6BKKElstsA-u0g7E-ZItBiza_fU9GPDLL4HBgBZCbDAjwAylNRzWcozAXMIAHvEN78MwmbHt5n_QUejSdeqBenfXYXIU5GczNpp9UPj2-QLAod0tYSCC89llUHl5KRMdaUpzKWFApepRhtZFoqkE-09JH5AZESiLunmggqy5g",
-//         accessToken:
-//           "eyJhbGciOiJSUzI1NiIsImtpZCI6ImUyYjIyZmQ0N2VkZTY4MmY2OGZhY2NmZTdjNGNmNWIxMWIxMmI1NGIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdGVhbTYtYWR2ZW50LWNhbGVuZGVyIiwiYXVkIjoidGVhbTYtYWR2ZW50LWNhbGVuZGVyIiwiYXV0aF90aW1lIjoxNzE1Njg0NjA0LCJ1c2VyX2lkIjoiZGVaQVluZEREWFd3d2JYNkJWNlBqdWhBcEZPMiIsInN1YiI6ImRlWkFZbmRERFhXd3diWDZCVjZQanVoQXBGTzIiLCJpYXQiOjE3MTU2ODQ2MDQsImV4cCI6MTcxNTY4ODIwNCwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ0ZXN0QGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.fvSimDz2lJc_-KjxbXoXRn1J45kO8wWGsi4bKjOtNnDG9h011zg0yCmGydYmVB40romPag7cPs0KWjHm-yi7jjD_c4druQ_FwDQZf3I5-bPSTXabagLB5XG8_w7Sg5jBvacOKKtWoNLXZtXI6k4gCoDrpP6zewsV7DzPNpIvDfZT3fSCzmuU8DgoiH1Yc78D7bfB4H_-fz6_oCNtCqaeVpUNHXt36zNnmIaKMS7cGPgZDy-Zy7e9xYtPybj9eYkOwdG1n6JDvSTs097eBSdbJXT35Taqeqoi5e1p5yKY6PzBslzeZ8Hw-nQGNIjYmLtr1q4tE0902ZVYfrGJi82I7Q",
-//         expirationTime: 1715688204487,
-//       },
-//       createdAt: "1712830615450",
-//       lastLoginAt: "1715684604423",
-//       apiKey: "AIzaSyAmcUY0zWSTqwSpUFuPwooCwjgjDBNkxls",
-//       appName: "[DEFAULT]",
-//     },
-//     loggedUserName: "test",
-//   },
-// };
-
 export { mockResponse };
