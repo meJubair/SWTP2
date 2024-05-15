00baddc558649929a19126909c4080a0829a561d
JerryUusis <jerry.uusitalo@gmail.com>
2024-05-15 18:26:46 +0300

    Implement ModalDialog for publishing a calendar

--- frontend/src/routes/EditorViewMain.tsx
+++ frontend/src/routes/EditorViewMain.tsx
@@ -6,6 +6,7 @@ import DateSelector from "../components/DateSelector";
 import BackgroundColourSelector from "../components/BackgroundColourSelector";
 import UploadImage from "../components/UploadImage";
 import Button from "@mui/material/Button";
+import ModalDialog from "../components/ModalDialog";
 import { useDispatch, useSelector } from "react-redux";
 import {
   ReduxCalendarState,
@@ -49,6 +50,9 @@ const EditorViewMain: React.FC = () => {
   const [showImageUpload, setShowImageUpload] = useState<boolean>(false);
   const [uploadedImageUrl, setUploadedImageUrl] = useState<string | null>(null);
   const [singleTag, setSingleTag] = useState<string>("");
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [dialogText, setDialogText] = useState<string>("");
+  const [dialogTitle, setDialogTitle] = useState<string>("");
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -251,9 +255,11 @@ const EditorViewMain: React.FC = () => {
     dispatch(setCalendarBackgroundUrl({ calendarIndex, newBackgroundUrl: "" }));
   };
 
+  // If user clicked yes on ModalDialog then close modal and run function
   // If publish status is false then post published_calendars to published collection.
   // Else remove from published_calendars collection
   const handlePublish = async () => {
+    setOpenModal(false);
     try {
       if (!calendar.published) {
         dispatch(setPublishStatus({ calendarIndex, newPublishStatus: true }));
@@ -269,8 +275,30 @@ const EditorViewMain: React.FC = () => {
     }
   };
 
+  // Set the content for modal
+  const confirmPublishCalendar = () => {
+    setOpenModal(true);
+    setDialogTitle("Publish calendar");
+    setDialogText(
+      `Are you sure you want to ${
+        calendar.published ? "unpublish" : "publish"
+      } this calendar?`
+    );
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <Box>
+      <ModalDialog
+        open={openModal}
+        titleText={dialogTitle}
+        dialogText={dialogText}
+        onConfirm={handlePublish}
+        onCancel={handleCloseModal}
+      />
       <Box
         sx={{
           width: "100%",
@@ -413,7 +441,11 @@ const EditorViewMain: React.FC = () => {
             )}
           </Box>
         </Box>
-        <Button variant="contained" sx={{ my: "1rem" }} onClick={handlePublish}>
+        <Button
+          variant="contained"
+          sx={{ my: "1rem" }}
+          onClick={confirmPublishCalendar}
+        >
           {calendar.published ? "Unpublish calendar" : "Publish calendar"}
         </Button>
         <Typography
