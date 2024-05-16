53884ef0cb1fbc82d72df66ae5d383655726c4d1
George Uzoh <ugochukwuuzoh@gmail.com>
2024-05-16 10:14:59 +0300

    add path to published calendar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import { Provider } from "react-redux";
 import EditorViewSingle from "./routes/EditorViewSingle";
 import ProtectedRoute from "./routes/ProtectedRoutes";
 import AuthLoader from "./components/AuthLoader";
+import PublishedCalendar from "./routes/PublishedCalendar";
 
 const App: React.FC = () => {
   return (
@@ -45,6 +46,10 @@ const App: React.FC = () => {
                 element={<EditorViewSingle />}
               />
             </Route>
+            <Route
+              path="/calendars/published/:uid/:calendarId"
+              element={<PublishedCalendar />}
+            />
           </Routes>
         </Router>
       </Provider>
